5cf384b883688438e9f1e3c5cb87934a6bcb71c9
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-08-05 17:17:48 -0500

    update

---
+++ src/components/App/App.jsx
@@ -0,0 +1,40 @@
+import ContactForm from "../ContactForm/ContactForm";
+import ContactList from "../ContactList/ContactList";
+import { Container, Paper } from "@mui/material";
+import Filter from "../Filter/Filter";
+import { useSelector } from "react-redux";
+
+const App = () => {
+  const { isLoading, error } = useSelector((state) => state.contacts);
+
+  return (
+    <Container>
+      <Paper
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          gap: 1,
+          overflow: "hidden",
+          mt: 2,
+          p: 2,
+        }}
+      >
+        <h1>Phonebook</h1>
+        <Paper sx={{ p: 2 }}>
+          <ContactForm />
+        </Paper>
+        <Paper sx={{ p: 2 }}>
+          <h2 style={{ display: "flex", justifyContent: "center" }}>
+            Contacts
+          </h2>
+          <Filter />
+          {isLoading && !error && <p>Feching data...</p>}
+          <ContactList />
+        </Paper>
+      </Paper>
+    </Container>
+  );
+};
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { CssBaseline } from "@mui/material";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
+import App from "./components/App/App.jsx";
+import React from "react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
+        <CssBaseline />
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
