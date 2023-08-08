375aba2a3b4163cd84e8c35f7adb4898712d1b75
Edison Estival Restrepo Ospina <edisonestival@gmail.com>
2023-08-07 19:00:49 -0500

    update

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,6 +5,7 @@ import LoadingPage from "../pages/LOading";
 import { Container } from "@mui/material";
 import { Footer } from "./Footer/Footer";
 
+
 const Layout = () => {
   return (
     <Container maxWidth='md' style={{border: "1px solid gray", padding: "0px" }}>
